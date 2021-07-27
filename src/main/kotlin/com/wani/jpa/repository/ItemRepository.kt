3a487e70dfe2c61d3e8146e0e2ad69b1f73ffa0d
oen142 <oen142@naver.com>
2021-07-27 23:45:19 +0900

    # 상품 서비스 개발 완료

--- src/main/kotlin/com/wani/jpa/repository/ItemRepository.kt
+++ src/main/kotlin/com/wani/jpa/repository/ItemRepository.kt
@@ -1,4 +1,33 @@
 package com.wani.jpa.repository
 
-class ItemRepository {
+import com.wani.jpa.domain.item.Item
+import org.springframework.stereotype.Repository
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+@Repository
+class ItemRepository(
+    @PersistenceContext
+    private val em: EntityManager
+) {
+
+    fun save(item: Item) {
+        if (item.id == null) {
+            em.persist(item)
+        } else {
+            em.merge(item)
+        }
+    }
+
+    fun findOne(id: Long): Item {
+        return em.find(Item::class.java, id)
+    }
+
+    fun findAll(): List<Item> {
+        return em.createQuery(
+            """
+                SELECT i FROM Item i
+            """.trimIndent(), Item::class.java
+        ).resultList
+    }
 }
--- src/main/kotlin/com/wani/jpa/service/ItemService.kt
+++ src/main/kotlin/com/wani/jpa/service/ItemService.kt
@@ -1,4 +1,26 @@
 package com.wani.jpa.service
 
-class ItemService {
+import com.wani.jpa.domain.item.Item
+import com.wani.jpa.repository.ItemRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class ItemService(
+    private val itemRepository: ItemRepository
+) {
+
+    @Transactional
+    fun saveItem(item: Item) {
+        itemRepository.save(item)
+    }
+
+    fun findItem(id: Long): Item =
+        itemRepository.findOne(id)
+
+
+    fun findItems(): List<Item> =
+        itemRepository.findAll()
+
 }
